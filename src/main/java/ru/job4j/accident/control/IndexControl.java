4f45306aced57e8659524fdd3e7325f5de92502f
xocer <vitamin9292@mail.ru>
2021-04-04 20:53:04 +0300

    Загрузка данных в вид

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> strings = List.of("1", "2", "3");
+        model.addAttribute("strings", strings);
         return "index";
     }
 }

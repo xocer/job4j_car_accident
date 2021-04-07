f35a12d2be8550e9b18ec89f17e6d9774e27598b
xocer <vitamin9292@mail.ru>
2021-04-07 14:37:52 +0300

    Spring Data

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -3,19 +3,25 @@ package ru.job4j.accident.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.repository.AccidentHibernate;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexControl {
-    private final AccidentHibernate  accidents;
+    private final AccidentRepository accidents;
 
-    public IndexControl(AccidentHibernate accidents) {
+    public IndexControl(AccidentRepository accidents) {
         this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", accidents.getAll());
+        List<Accident> res = new ArrayList<>();
+        accidents.findAll().forEach(res::add);
+        model.addAttribute("accidents", res);
         return "index";
     }
 }
